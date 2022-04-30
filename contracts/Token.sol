600fe05d37bf037a3dca9c9a00468dc6f2912e3b
Ray Gu <rgu@cotnetwork.com>
2022-04-30 13:35:29 +0800

    add some files

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract Token is ERC20 {
-
+    constructor(uint totalSupply) ERC20("Token name", "Token Symbol") {
+        _mint(msg.sender, totalSupply);
+        console.log("The owner address is:", msg.sender);
+        console.log("Total supply is:", totalSupply);
+    }
 }
