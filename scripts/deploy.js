dbf1fb00217ffdde164f406f1457d96a6e1fa1b0
Ray Gu <rgu@cotnetwork.com>
2022-04-30 14:23:55 +0800

    ready for ERC20.

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy contracts here:
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy(100);
+
+  console.log("Token contract address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
