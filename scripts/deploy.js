c30fe3d85a7f26ae6df2f238e5c6d4a6b65f780e
Ray Gu <rgu@cotnetwork.com>
2022-05-02 14:47:16 +0800

    optimized a bit.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,26 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  // deploy contracts here:
   const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy('1000000000000000000000000'); // 1 million token, with decimal of 18
-
+  console.log("Deploying...")
+  const token = await Token.deploy('8888000000000000000000'); // 8888 token with 18 decimals
+  
+  await token.deployed();
   console.log("Token contract address:", token.address);
+  console.log("Token deployed to:", deployer.address);
+  console.log("Token name:", (await token.name()));
+  console.log("Token symbol:", (await token.symbol()));
+  console.log("Total supply:", (await token.totalSupply()));
+  console.log("Deployer's amount:", (await token.balanceOf(deployer.address))); 
 }
 
 main()
-  .then(() => process.exit(0))
+  .then(() => {
+    console.log(`Migration Succeed!`);
+    process.exit(0);
+  })
   .catch(error => {
     console.error(error);
     process.exit(1);
